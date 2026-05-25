46ffde2a694779042aaf27cc0824dfa574b8b6fc
Rupchandra <manharerupchandra2@gmail.com>
2026-05-25 08:33:35 +0530

    new fun added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,5 +8,5 @@ router.post("/add-bmi-record/:user_id",addBmiRecord);
 router.get('/',getAllUser)
 router.put('/update/:id',updateUser)
 router.delete('/deleteAll',deleteAllUser)
-router.get('/getHistory',getHistory)
+router.get('/getHistory/:id',getHistory)
 module.exports = router;
