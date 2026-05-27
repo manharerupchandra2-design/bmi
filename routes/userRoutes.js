965780f787c0b5361fd678c1757fe1fcac620281
Rupchandra <manharerupchandra2@gmail.com>
2026-05-27 05:40:54 +0530

    purana wala code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const {createUser, addBmiRecord, getAllUser, updateUser, deleteAllUser, getHistory , deleteHistory} = require('../controller/userController')
+const {createUser, 
+    addBmiRecord, 
+    getAllUser, 
+    updateUser,  
+    getHistory , 
+    deleteHistory} = require('../controller/userController')
 
 router.post("/create-user", createUser);
 router.post("/add-bmi-record/:user_id",addBmiRecord);
 router.get('/',getAllUser);
 router.put('/update/:id',updateUser);
-router.delete('/deleteAll',deleteAllUser);
 router.get('/getHistory/:id',getHistory);
 router.delete('/deleteHistory/:id',deleteHistory);
 module.exports = router;
